48ce27f96b3e7e30f51a4f26df67b1ccc1b5d881
yinruyi <916280067@qq.com>
2015-12-02 23:37:10 +0800

    test

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        int[] a = new int[] {1, 3, 5, 7, 9};
+        System.out.println(a);
+        System.out.println(7%2);
+    }
+}
---
+++ test/test.java
@@ -0,0 +1,19 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Declare in the middle:");
+        //int a;
+        //a = 5 + 1;
+        //System.out.println(a);  // print an integer
+        System.out.println(110 & 001);
+        int a;
+        a = 2013;
+        if ((a%4) == 0){
+        	System.out.println("runnian");
+        }
+        else {
+        	System.out.println("not");
+        }
+    }
+}
