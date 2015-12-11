73218e95c2f8ec6301b35e32453ec0767c40da08
R. Yin <916280067@qq.com>
2015-12-11 16:58:54 +0800

    up

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -16,7 +16,32 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Declare in the middle:");
+        Human aPerson = new Human();
+        System.out.println(aPerson.getHeight());
+        aPerson.growHeight(10);
+        System.out.println(aPerson.getHeight());
     }
     
+}
+
+
+class Human
+{
+/**
+     * accessor
+     */
+    int getHeight()
+    {
+        return this.height;
+    }
+
+    /**
+     * pass argument
+     */
+    void growHeight(int h)
+    {
+        this.height = this.height + h;
+    }
+
+    int height;
 }
